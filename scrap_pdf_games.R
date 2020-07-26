bdcbb266d02c461df384b089cf001bc3f24d441d
Adrian Cadena <adriancadenam93@gmail.com>
2020-07-26 16:06:38 -0500

    pushing_pdfs

---
+++ scrap_pdf_games.R
@@ -0,0 +1,31 @@
+library(readtext)
+library(pdftools)
+library(stringr)
+library(dplyr)
+
+################################3
+
+home_team_loop<-read.csv('home_teams.csv')
+
+############################################
+
+for (d in 57900:58155){
+  boolFalse<-F
+  i = 1
+  while(boolFalse==F){
+    ht = home_team_loop$x[i]
+    if (is.na(ht)){
+      next
+    }
+    url = paste0('https://nflcdns.nfl.com/liveupdate/gamecenter/',toString(d),'/',home_team_loop$x[i],'_Gamebook.pdf')
+    tryCatch({
+      download.file(url,paste0('C:/Users/adrian-boss/Desktop/games/',toString(d),'_',home_team_loop$x[i],'_Gamebook','_2019','.pdf'),mode="wb");
+      boolFalse<-T
+    },error=function(e){
+    },finally={})
+    i = i+1
+  }
+  
+}
+
+is.na(home_team_loop$x[33])
