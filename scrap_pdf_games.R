0924254a8cb80a5b244ed049f4f3dd0741d4122f
Adrian Cadena <adriancadenam93@gmail.com>
2020-07-27 20:51:48 -0500

    corrected_season19

--- scrap_pdf_games.R
+++ scrap_pdf_games.R
@@ -3,12 +3,13 @@ library(pdftools)
 library(stringr)
 library(dplyr)
 
-################################3
+#################################
 
 home_team_loop<-read.csv('home_teams.csv')
 
 ############################################
-
+# 2019
+#########################
 for (d in 57900:58155){
   boolFalse<-F
   i = 1
@@ -19,7 +20,28 @@ for (d in 57900:58155){
     }
     url = paste0('https://nflcdns.nfl.com/liveupdate/gamecenter/',toString(d),'/',home_team_loop$x[i],'_Gamebook.pdf')
     tryCatch({
-      download.file(url,paste0('C:/Users/adrian-boss/Desktop/games/',toString(d),'_',home_team_loop$x[i],'_Gamebook','_2019','.pdf'),mode="wb");
+      download.file(url,paste0('C:/Users/adrian-boss/Documents/GitHub/Scraping_NFL_gamebooks/pdfs/2019/',toString(d),'_',home_team_loop$x[i],'_Gamebook','_2019','.pdf'),mode="wb");
+      boolFalse<-T
+    },error=function(e){
+    },finally={})
+    i = i+1
+  }
+}
+
+############################################
+# 2018
+#########################
+for (d in 57567:57822){
+  boolFalse<-F
+  i = 1
+  while(boolFalse==F){
+    ht = home_team_loop$x[i]
+    if (is.na(ht)){
+      break
+    }
+    url = paste0('https://nflcdns.nfl.com/liveupdate/gamecenter/',toString(d),'/',home_team_loop$x[i],'_Gamebook.pdf')
+    tryCatch({
+      download.file(url,paste0('C:/Users/adrian-boss/Documents/GitHub/Scraping_NFL_gamebooks/pdfs/2018/',toString(d),'_',home_team_loop$x[i],'_Gamebook','_2018','.pdf'),mode="wb");
       boolFalse<-T
     },error=function(e){
     },finally={})
